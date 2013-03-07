d9639e6381f3fc0aa894c0269b69b0e30efc839f
Pete <pete.albrecht@gmail.com>
2013-03-07 17:07:23 -0500

    require login to save notes

--- application/views/dash.php
+++ application/views/dash.php
@@ -4,7 +4,7 @@
     <?php if ($this->ion_auth->logged_in()): ?>
     <p>Welcome <?php echo $this->ion_auth->user()->row()->username; ?>! <a href="auth/logout">Logout</a></p>
     <?php else: ?>
-    <a href="../auth/login">Login</a>
+    <p>Welcome! Would you like to log in? <a href="../auth/login">Login</a></p>
     <?php endif; ?>
 </div>
 
--- application/views/notepad.php
+++ application/views/notepad.php
@@ -1,22 +1,16 @@
 <?php
-
 $this->load->helper('form');
 echo form_open('notes/save');
+?>
 
-echo form_input(array(
-    'name' => 'title',
-    'id' => 'title',
-    'maxlength' => '64'
-    ));
+<input type="text" name="title" value="" id="title" maxlength="64"  />
+<textarea name="notes" cols="85" rows="30" id="notes" ></textarea>
+<?php if ($this->ion_auth->logged_in()): ?>
+<input type="submit" name="save" value="Save"  />
+<?php else: ?>
+<input type="submit" name="save" value="Save" disabled="disabled" />
+<?php endif; ?>
 
-echo form_textarea(array(
-    'name' => 'notes',
-    'id' => 'notes',
-    'rows' => '35',
-    'cols' => '85'
-    ));
-
-echo form_submit('save', 'Save');
+<?php
 echo form_close();
-
 ?>
