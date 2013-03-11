eef0e0094fd7ec4fd68d289a2a3587f0d0d100ed
Pete <pete.albrecht@gmail.com>
2013-03-11 14:16:51 -0400

    transform note text to html in view. added "notes/edit" link

--- application/views/notebook.php
+++ application/views/notebook.php
@@ -1,6 +1,7 @@
 <?php foreach($notes as $note): ?>
 <div>
 <h3><?php echo $note['title']; ?></h3>
-<p><?php echo $note['text']; ?></p>
+<p><?php echo $this->markdown->transform($note['text']); ?></p>
+<a href="notes/edit/<?php echo $note['id']; ?>">Edit</a>
 </div>
 <?php endforeach; ?>
